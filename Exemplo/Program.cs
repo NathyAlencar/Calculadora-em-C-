8c84b4e2e84e7a996099441467fbfc420a6d4565
Nathalia Maisa Alencar de Araujo <136849458+NathyAlencar@users.noreply.github.com>
2024-07-02 11:14:34 -0300

    create calculadora

---
+++ Exemplo/Program.cs
@@ -0,0 +1,16 @@
+﻿using Exemplo.models;
+
+
+Calculadora clac = new Calculadora();
+
+clac.Somar(10, 30);
+clac.Subtrair(10, 50);
+clac.Multiplicar(15, 45);
+clac.Dividir(2, 2);
+clac.Potencia(3, 3);
+clac.Seno(30);
+clac.Coseno(30);
+clac.Tangente(30);
+clac.RaizQuadrada(9);
+
+
